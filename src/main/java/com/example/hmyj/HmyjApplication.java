63fe8e8a9e6bc51cfabe502b0c409d110dfc17aa
xiechao <xiechao01@fenbi.com>
2019-03-08 16:04:22 +0800

    Initial commit

---
+++ src/main/java/com/example/hmyj/HmyjApplication.java
@@ -0,0 +1,13 @@
+package com.example.hmyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmyjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmyjApplication.class, args);
+    }
+
+}
